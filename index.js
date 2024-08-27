899d032e8b071e2237e036b7253c3f1678225d79
Evgvakh <evgvakh@gmail.com>
2024-08-27 21:33:36 +0200

    6th

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function setCorsHeaders(req, res, next) {
 const app = express();
 app.use(express.json());
 app.use(setCorsHeaders)
-app.use(cors());
+// app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
 connectToDB();
