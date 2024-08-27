f31656b54b4179162b4c1fcfb6f51232ee751efb
Evgvakh <evgvakh@gmail.com>
2024-08-27 22:25:22 +0200

    11th

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const corsOptions = {
 const app = express();
 app.use(express.json());
 app.use(cors(corsOptions));
+app.options('*', cors());
 app.use("/uploads", express.static("uploads"));
 
 connectToDB();
